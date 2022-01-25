c6207f815335018fa118879e9d10e676a1643b36
fatihsygn07 <fsaygin2504@gmail.com>
2022-01-25 15:55:04 +0300

    Angular Odev
    Butunleme odevi

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyADn3ixA7UDXE3U6wOUdSy3g1yr0sQUauQ",
+    authDomain: "teknoform-2a054.firebaseapp.com",
+    databaseURL: "https://teknoform-2a054-default-rtdb.firebaseio.com",
+    projectId: "teknoform-2a054",
+    storageBucket: "teknoform-2a054.appspot.com",
+    messagingSenderId: "1021396356046",
+    appId: "1:1021396356046:web:135798e16399b50b7a799f",
+    measurementId: "G-VKSDBWXW6G"
+  }
+};
